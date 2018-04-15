d052e91955b384b67909a13d620e298d810f1cd3
Ivan Lim <aimprove@gmail.com>
2018-04-15 16:37:06 -0400

    Fix browser resize problem

--- components/d3-force-directed-graph.js
+++ components/d3-force-directed-graph.js
@@ -7,6 +7,7 @@ Vue.component('d3-force-directed-graph', {
     return {
       canvas: false,
       context: false,
+      simulation: false,
       nodeRadius: 5,
       id: 'd3-' + Math.round(Math.random() * 1000000)
     }
@@ -32,41 +33,70 @@ Vue.component('d3-force-directed-graph', {
      * Data is now available to build structure of graph
      */
     d3Data () {
-      this.drawData();
+      this.simulation = d3
+        .forceSimulation()
+        .force('x', d3.forceX(this.graphWidth / 2))
+        .force('y', d3.forceY(this.graphHeight / 2))
+        // collide will prevent nodes from overlapping
+        .force('collide', d3.forceCollide(this.nodeRadius + 5))
+        // charge will instruct nodes to repulse each other
+        .force('charge', d3
+          .forceManyBody()
+          .strength(-70)
+        )
+        // Mapping array links and nodes
+        .force('link', d3
+          .forceLink()
+          // Indicate which field in nodes is source mapping to
+          // .id(d => d.country)
+        )
+        // Setting decay to 0 simulation will never stop
+        // .alphaDecay(0)
+        .on('tick', this.update);
+      
+      // Ref: https://github.com/d3/d3-force
+      // Once simulation is executed, x, y, vy, vx will be added to nodes
+      this.simulation
+        .nodes(this.d3Data.nodes)
+        // Passing data to the links network
+        .force('link')
+        .links(this.d3Data.links);
+      
+      this.enableDrag();
     },
     graphHeight () {
-      if (this.d3Data.length > 0) this.drawData();
+      this.restartSimulation();
     },
     graphWidth () {
-      if (this.d3Data.length > 0) this.drawData();
+      this.restartSimulation();
     },
   },
   methods: {
-    enableDrag (simulation) {
-      let dragsubject = () => simulation.find(d3.event.x, d3.event.y);
+    enableDrag () {
+      let dragsubject = () => this.simulation.find(d3.event.x, d3.event.y);
       let dragstarted = () => {
-        if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+        if (!d3.event.active) this.simulation.alphaTarget(0.3).restart();
         d3.event.subject.fx = d3.event.subject.x;
         d3.event.subject.fy = d3.event.subject.y;
-        console.log('remove', d3.event.subject);
       }
       let dragged = () => {
         d3.event.subject.fx = d3.event.x;
         d3.event.subject.fy = d3.event.y;
       }
       let dragended = () => {
-        if (!d3.event.active) simulation.alphaTarget(0);
+        if (!d3.event.active) this.simulation.alphaTarget(0);
         d3.event.subject.fx = null;
         d3.event.subject.fy = null;
       }
 
-      this.canvas
-        .call(d3.drag()
+      this.canvas.call(
+        d3.drag()
           .container(this.canvas.node())
           .subject(dragsubject)
           .on("start", dragstarted)
           .on("drag", dragged)
-          .on("end", dragended));
+          .on("end", dragended)
+      );
     },
     drawLink (link) {
       this.context.moveTo(link.source.x, link.source.y);
@@ -91,7 +121,6 @@ Vue.component('d3-force-directed-graph', {
       let img = new Image(sprite.width, sprite.height);   
       img.src = "./assets/img/flags.png";
       img.className = `flag flag-${node.code}`;
-      img.alt = 'test';
       let computedStyle = window.getComputedStyle(img);
 
       this.context.drawImage(
@@ -106,6 +135,14 @@ Vue.component('d3-force-directed-graph', {
         sprite.height
       );
     },
+    restartSimulation () {
+      if (!this.simulation) return;
+
+      this.simulation
+        .force('center', d3.forceCenter(this.graphWidth / 2, this.graphHeight / 2))
+        .alpha(1)
+        .restart();
+    },
     update () {
       this.context.clearRect(0, 0, this.graphWidth, this.graphHeight);
     
@@ -118,48 +155,11 @@ Vue.component('d3-force-directed-graph', {
       this.d3Data.links.forEach(this.drawLink);
       this.context.stroke();
     },
-    /**
-     * Draw d3Data
-     */
-    drawData () {
-      let simulation = d3
-        .forceSimulation()
-        .force('x', d3.forceX(this.graphWidth / 2))
-        .force('y', d3.forceY(this.graphHeight / 2))
-        // collide will prevent nodes from overlapping
-        .force('collide', d3.forceCollide(this.nodeRadius + 5))
-        // charge will instruct nodes to repulse each other
-        .force('charge', d3
-          .forceManyBody()
-          .strength(-70)
-        )
-        // Mapping array links and nodes
-        .force('link', d3
-          .forceLink()
-          // Indicate which field in nodes is source mapping to
-          // .id(d => d.country)
-        )
-        // Setting decay to 0 simulation will never stop
-        // .alphaDecay(0)
-        .on('tick', this.update);
-      
-      // Ref: https://github.com/d3/d3-force
-      // Once simulation is executed, x, y, vy, vx will be added to nodes
-      simulation
-        .nodes(this.d3Data.nodes)
-        // Passing data to the links network
-        .force('link')
-        .links(this.d3Data.links);
-      
-      this.enableDrag(simulation);
-    },
   },
   mounted () {
     // Step #1: Select div to place d3 chart, set dimensions and color
     // Note: Code below must be in mounted(), created() does not work
     this.canvas = d3.select(`#${this.id}`);
     this.context = this.canvas.node().getContext('2d');
-
-
   },
 });
